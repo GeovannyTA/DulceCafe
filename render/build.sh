6aef093348e33d4137b48c00d881440809bc77b5
Geovanny Araujo <trejogeovannyaraujo@gmail.com>
2023-10-29 14:40:12 -0600

    render

--- render/build.sh
+++ render/build.sh
@@ -3,8 +3,6 @@
 set -o errexit
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
-/opt/render/project/src/.venv/bin/python -m apt update
-/opt/render/project/src/.venv/bin/python -m apt upgrade python3
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-imput
