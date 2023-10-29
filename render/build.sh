fc318bf41fc931487e7ec14fca3459345af054e8
Geovanny Araujo <trejogeovannyaraujo@gmail.com>
2023-10-29 14:32:21 -0600

    upgrade

--- render/build.sh
+++ render/build.sh
@@ -3,6 +3,9 @@
 set -o errexit
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+/opt/render/project/src/.venv/bin/python -m sudo apt update
+/opt/render/project/src/.venv/bin/python -m sudo apt upgrade python3
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-imput
 python manage.py migrate
