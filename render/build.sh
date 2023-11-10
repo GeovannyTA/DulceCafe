e2786b4a679275a2057d8dca6a10dba55e4c13bd
GeovannyTA <trejogeovannyaraujo@gmail.com>
2023-11-09 19:26:09 -0600

    build

--- render/build.sh
+++ render/build.sh
@@ -5,5 +5,5 @@ set -o errexit
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 
 pip install -r requirements.txt
-python manage.py collectstatic --noimput
+python manage.py collectstatic --noinput
 python manage.py migrate
